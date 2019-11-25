7f87b31cdcde338216c66ab0b295493ad5e1b5ef
pierreb-ys <57370760+pierreb-ys@users.noreply.github.com>
2019-11-25 13:38:56 -0500

    Add/Edit Newsfeed Post Updated

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,3 +1,25 @@
 $(function () {
-    $(".chosen").chosen()
-})
+    $('.chosen').chosen();
+});
+
+$(function () {
+    $('#summernote').summernote();
+});
+
+$(function () {
+
+
+    jQuery('#datetimepicker').datetimepicker({
+        format:'d.m.Y H:i',
+        lang:'ru'
+    });
+
+    jQuery('#image_button').click(function(){
+        jQuery('#datetimepicker').datetimepicker('show'); //support hide,show and destroy command
+    });
+
+});
+
+//$(document).ready(function() {
+//    $("#summernote").summernote();
+//});
