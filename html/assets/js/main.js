d3f50c5f16e53ca8bba0c4449b7cb97f4fd78571
pierreb-ys <57370760+pierreb-ys@users.noreply.github.com>
2020-01-29 22:12:03 -0500

    Disable Sub-School When All Schools is Selected

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -24,6 +24,7 @@ $(function () {
 //    $("#summernote").summernote();
 //});
 
+//
 function setPublishDate() {
 
    if (document.getElementById("publish-checkbox").checked == true) {
@@ -34,3 +35,38 @@ function setPublishDate() {
        document.getElementById("publish-date-text").innerHTML = "dd/mm/yyyy";
    }
  }
+
+
+
+// 4-B-Catalog-Curriculum-Detail
+function disableSubSchools() {
+
+   if (document.getElementById("school0").checked == true) {
+   
+      document.getElementById("subSchool0").disabled = true;
+      document.getElementById("subSchool1").disabled = true;
+      document.getElementById("subSchool2").disabled = true;
+      document.getElementById("subSchool3").disabled = true;
+      document.getElementById("subSchool4").disabled = true;
+      document.getElementById("subSchool5").disabled = true;
+      document.getElementById("subSchool6").disabled = true;
+      document.getElementById("subSchool7").disabled = true;
+      document.getElementById("subSchool8").disabled = true;
+
+   }
+
+   else if (document.getElementById("school0").checked == false) {
+
+      document.getElementById("subSchool0").disabled = false;
+      document.getElementById("subSchool1").disabled = false;
+      document.getElementById("subSchool2").disabled = false;
+      document.getElementById("subSchool3").disabled = false;
+      document.getElementById("subSchool4").disabled = false;
+      document.getElementById("subSchool5").disabled = false;
+      document.getElementById("subSchool6").disabled = false;
+      document.getElementById("subSchool7").disabled = false;
+      document.getElementById("subSchool8").disabled = false;
+
+   }
+
+}
